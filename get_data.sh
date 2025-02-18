7e4653d81459aa4a40646c1e9ff59d3d0e66bee6
ymx <yuming.x@outlook.com>
2025-02-18 18:42:24 +0800

    fix get_data.sh

--- get_data.sh
+++ get_data.sh
@@ -5,8 +5,15 @@ DATA_ROOT=$BASH_ROOT$DATA_SUBDIR
 echo "BASH_ROOT=$BASH_ROOT"
 echo "PWD=$PWD"
 if [ ! -d $DATA_ROOT ]; then
-	if [ ! -f $BASH_ROOT/all.gpgpu-sim-app-data.tgz ] &&[ ! -f $BASH_ROOT/../all.gpgpu-sim-app-data.tgz ] ; then
-		wget https://engineering.purdue.edu/tgrogers/gpgpu-sim/benchmark_data/all.gpgpu-sim-app-data.tgz
+	# 定义可能的文件路径
+	if [ -f $BASH_ROOT/all.gpgpu-sim-app-data.tgz ]; then
+	    TAR_FILE=$BASH_ROOT/all.gpgpu-sim-app-data.tgz
+	elif [ -f $BASH_ROOT/../all.gpgpu-sim-app-data.tgz ]; then
+	    TAR_FILE=$BASH_ROOT/../all.gpgpu-sim-app-data.tgz
+	else
+	    # 如果文件不存在，则下载文件到 $BASH_ROOT
+	    wget https://engineering.purdue.edu/tgrogers/gpgpu-sim/benchmark_data/all.gpgpu-sim-app-data.tgz -O $BASH_ROOT/all.gpgpu-sim-app-data.tgz
+	    TAR_FILE=$BASH_ROOT/all.gpgpu-sim-app-data.tgz
 	fi
-    tar xzvf all.gpgpu-sim-app-data.tgz -C $BASH_ROOT
+	tar xzvf $TAR_FILE -C $BASH_ROOT
 fi
